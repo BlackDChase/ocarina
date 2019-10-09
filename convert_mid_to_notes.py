41f2f5ed4a3fa30842fdfba81ab28926ec5bccf4
Mrigyen Sawant <Mrigyen@users.noreply.github.com>
2019-10-10 01:16:54 +0530

    add main dataset and initial script to convert midi files to actual notes (data wrangling)

---
+++ convert_mid_to_notes.py
@@ -0,0 +1,27 @@
+import glob
+from music21 import converter, instrument, note, chord
+
+notes = []
+
+for file in glob.glob("ocarina_midi_data/*.mid"):
+    try:
+        midi = converter.parse(file)
+    except:
+        print(str(file), "is bad")
+        continue
+    notes_to_parse = None
+    parts = instrument.partitionByInstrument(midi)
+    if parts:  # file has instrument parts
+        notes_to_parse = parts.parts[0].recurse()
+    else:  # file has notes in a flat structure
+        notes_to_parse = midi.flat.notes
+
+    for element in notes_to_parse:
+        if isinstance(element, note.Note):
+            notes.append(str(element.pitch))
+        elif isinstance(element, chord.Chord):
+            notes.append('.'.join(str(n) for n in element.normalOrder))
+
+print(notes)
+with open("data/ocarina.txt", "w+") as file:
+    file.write(notes)
