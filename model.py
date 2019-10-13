98f1f8ad5c9dbf0dcab054ca9326065d2295e743
BlackDChase <blackdragonchase@gmail.com>
2019-10-13 18:50:43 +0530

    Input given to tokeniser is having issue
    Converter Modifed for many files interpretation
    
    Input Data taken in better way

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch as pt
 from random import random
-
+import sys
 
 if pt.cuda.is_available():
     device = pt.device("cuda:0")
@@ -11,8 +11,11 @@ dic = {}
 #tokenize data dict
 
 def tokenize(data):
+    print("yo",data)
     Data = []
     for i in data:
+        print("\n>>",i)
+        sys.exit()
         if i not in dic.keys():
             x = 1
             while x in dic.values():
@@ -20,47 +23,55 @@ def tokenize(data):
             dic[i] = x
         Data.append(dic[i])
     return Data
+#Using random Function to make data more distinct
+
 
 def getData(file_n,n):
-    in_data = []
-    DATA = []
-    notes = open(file_n,"r")
-    for lines in notes:
-        DATA.append(lines)
-    
-    in_data = DATA[0] 
-    #right now all the songs are in one line so... 
-    
-    F_DATA = []
-    #for in_data in DATA:
-    # when many songs seprated by lines
-    
-    data = []
-    for i in range(1,len(in_data)-1):
-        alph = ''
-        while (in_data[i]!='\'' or in_data[i]!=' ' or in_data[i]!=',') and i>(len(in_data)-1):
-            alph = alph + in_data[i]
-            i = i + 1
-        data.append(alph)
-    data = tokenize(data)
-    final_data = []
-    for i in range(0, len(data),n):
-        alph = []
-        for z in range(i,i+n):
-            if z>=len(data):
-                break
-            alph.append(data[z])
-        final_data.append(alph)
-    
-    if len(final_data[-2])>len(final_data[-1]):
-        del final_data[-1]
-    #F_DATA.append(final_data)
-    #for loop to be closes for many song data
+   # in_data = []
 
-    F_DATA = in_data
-    F_DATA =  pt.Tensor(F_DATA)
-    F_DATA.to(device)
-    return F_DATA
+    notes = []
+    #Every line will have notes of 1 midi file
+
+    f_notes = open(file_n,"r")
+    for lines in f_notes:
+        notes.append(lines)
+    #print(len(notes)) 
+    inputData = []
+    for in_data in notes:
+        # when many songs seprated by lines
+        if len(in_data) < (n+(n//2)):
+            continue
+        music = []
+        for i in range(0,len(in_data)):
+            beat = ''
+            while (in_data[i]!='\'' or in_data[i]!=' ' or in_data[i]!=',' or in_data[i]!='[' or in_data[i]!=']') and i>(len(in_data)-1):
+                beat = beat + in_data[i]
+                i = i + 1
+            music.append(beat)
+        nMusic = tokenize(music)
+        music = []
+        # len(music) is number of label available for each music
+        for i in range(len(nMusic) - n):
+            workset = []
+            for j in range(i,i+n):
+                workset.append(nMusic[j])
+            music.append(workset)
+        for i in range(0,(len(music)//(n//2 + 1)),(n//2 + 1) ):
+            label = []
+            if (i + n//2 + 1)>len(music):
+                continue
+            for k in range(i,i+(n//2)+1):
+                label.append(music[k])
+            inputData.append(label)
+    #for loop to be closes for many song data
+    
+    print(len(inputData),len(inputData[0]),len(inputData[0][0]),inputData[0][0])
+    print(type(inputData),type(inputData[0]),type(inputData[0][0]))
+    print(dic)
+    #inputData =  pt.Tensor(inputData)
+    #print(inputData.size(),type(inputData))
+    #inputData.to(device)
+    return inputData
 
 file_n="data/ocarina.txt"
 n=100 
