0c93b6bd42190007a7dee0f27a0e6c5964f5089f
BlackDChase <blackdragonchase@gmail.com>
2019-12-04 01:39:11 +0530

    Extra Function added, so all can be done through 1 file

--- convert_mid_to_notes.py
+++ convert_mid_to_notes.py
@@ -3,30 +3,6 @@ from music21 import converter, instrument, note, chord
 
 notes = []
 
-'''
-for file in glob.glob("ocarina_midi_data/*.mid"):
-    try:
-        midi = converter.parse(file)
-    except:
-        print(str(file), "is bad")
-        continue
-    notes_to_parse = None
-    parts = instrument.partitionByInstrument(midi)
-    if parts:  # file has instrument parts
-        notes_to_parse = parts.parts[0].recurse()
-    else:  # file has notes in a flat structure
-        notes_to_parse = midi.flat.notes
-
-    for element in notes_to_parse:
-        if isinstance(element, note.Note):
-            notes.append(str(element.pitch))
-        elif isinstance(element, chord.Chord):
-            notes.append('.'.join(str(n) for n in element.normalOrder))
-print(notes)
-with open("data/ocarina.txt", "w+") as file:
-    file.write(str(notes))
-#this is for a combined output
-'''
 #this is for an output which is seprated by diffrent music files because every file is different
 
 for files in glob.glob("ocarina_midi_data/*.mid"):
@@ -38,21 +14,56 @@ for files in glob.glob("ocarina_midi_data/*.mid"):
         continue
     notes_to_parse = None
     parts = instrument.partitionByInstrument(midi)
-    if parts:  # file has instrument parts
+    if parts:  
+        # file has instrument parts
         notes_to_parse = parts.parts[0].recurse()
-    else:  # file has notes in a flat structure
+    else:
+        # file has notes in a flat structure
         notes_to_parse = midi.flat.notes
-
     for element in notes_to_parse:
         if isinstance(element, note.Note):
             musicnote.append(str(element.pitch))
         elif isinstance(element, chord.Chord):
             for cord in element.normalOrder:
-                musicnote.append('>' + (str(cord)))
+                musicnote.append(str(cord))
+    
     if len(musicnote)>0:
         notes.append(musicnote)
-print(notes)
+        #print(musicnote)
+    #print("To stop Give 0 : ",end=' ')
+    #stopNot = input()
+    #if stopNot=='0':
+    #    break
+
 with open("data/ocarina.txt", "w+") as file:
-    for i in range(len(notes)):
-        file.write(str(notes[i]))
+    for i in notes:
+        file.write(str(i))
         file.write("\n")
+
+def dataLoader():
+    notes = []
+    #this is for an output which is seprated by diffrent music files because every file is different
+    for files in glob.glob("ocarina_midi_data/*.mid"):
+        musicnote = []
+        try:
+            midi = converter.parse(files)
+        except:
+            continue
+        notes_to_parse = None
+        parts = instrument.partitionByInstrument(midi)
+        if parts:  
+            # file has instrument parts
+            notes_to_parse = parts.parts[0].recurse()
+        else:
+            # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                musicnote.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                for cord in element.normalOrder:
+                    musicnote.append(str(cord))
+        
+        if len(musicnote)>0:
+            notes.append(musicnote)
+    return notes
