495c10e1cd9ffa82d364471339ac9c91eec05295
BlackDChase <blackdragonchase@gmail.com>
2019-12-06 23:46:32 +0530

    Enhanced the working

--- midi_out.py
+++ midi_out.py
@@ -4,14 +4,13 @@ def note_finder(data,dic):
     for key in dic.keys():
         if dic[key]==data:
             return key
+    return None
 
 def refurbished(data,dic):
-    if data in dic.values():
-        return note_finder(data,dic)
-    temp = []
-    for lists in data:
-        temp.append(refurbished(lists,dic))
-    return temp
+    refurbished_data = []
+    for tokened_note in data:
+        refurbished_data.append(note_finder(tokened_note,dic))
+    return refurbished_data
 
 def make_music(data):
     if(type(data[0])!=list):
@@ -22,5 +21,9 @@ def make_music(data):
 
 def prep_files(data,dic):
     refurbished_data = refurbished(data,dic)
+    #print(dic)
+    #print(len(data),len(data[0]),data[0])
     print("Making Music")
-    make_music(refurbished_data)
+    print(refurbished_data)
+    music = make_music(refurbished_data)
+    return music
